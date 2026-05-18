bd929346c15dded2b7100bb815853668c84491ee
Adela717 <adelamihaela17@yahoo.com>
2026-05-18 16:36:00 +0300

    database connection

--- app/main.py
+++ app/main.py
@@ -1,7 +1,24 @@
 from fastapi import FastAPI
+from app.db import get_connection
 
 app = FastAPI(title="Database Performance Monitor")
 
 @app.get("/")
 def home():
     return {"message": "Database Performance Monitor is running"}
+
+@app.get("/db-test")
+def db_test():
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT version();")
+    version = cursor.fetchone()[0]
+
+    cursor.close()
+    conn.close()
+
+    return {
+        "database_connection": "successful",
+        "postgres_version": version
+    }
